1994177b0e5660f10ab98153947803eaf69a93d2
Shiven Khurana <144283263+ShivenKhurana1@users.noreply.github.com>
2024-04-01 12:07:55 -0400

    Update 3_Find Doctors.py

--- pages/3_Find Doctors.py
+++ pages/3_Find Doctors.py
@@ -1,15 +1,4 @@
 import streamlit as st
-from geopy.geocoders import Nominatim
-import requests
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import requests
-import seaborn as sns
-from transformers import pipeline
-from PIL import Image
-from dotenv import load_dotenv
-import os
 
 
 st.set_page_config(
